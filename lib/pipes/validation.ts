b510716833bdd2d9830c0618850232cb7075cb12
Vlad <vladbolkunovv@gmail.com>
2025-01-18 21:58:00 +0300

    feat(pipes/validation): zod support & class-transforming for class-validator

--- lib/pipes/validation.ts
+++ lib/pipes/validation.ts
@@ -1,7 +1,10 @@
 import { ClassSchema } from '../core';
 import Joi from 'joi';
 import { pipe, Pipe } from './core';
-import { validate, ValidationError } from 'class-validator';
+import { validate, ValidationError, ValidatorOptions } from 'class-validator';
+import { z } from 'zod';
+import 'reflect-metadata';
+import { ClassTransformOptions, plainToInstance } from 'class-transformer';
 
 export class ClassValidatorError extends Error {
   name = 'ValidationError';
@@ -10,32 +13,61 @@ export class ClassValidatorError extends Error {
   }
 }
 
+export interface ClassParams {
+  transform: Partial<ClassTransformOptions>;
+  validation: Partial<ValidatorOptions>;
+}
+
+export function validatePipe<C extends object>(
+  schema: z.Schema<C>,
+  params?: Partial<z.ParseParams>,
+): Pipe<any, Promise<C | z.ZodError>>;
+
 export function validatePipe<C extends object>(
   schema: ClassSchema<C>,
+  params?: ClassParams,
 ): Pipe<any, Promise<C | ClassValidatorError>>;
 
 export function validatePipe<C>(
   schema: Joi.Schema<C>,
+  params?: Joi.AsyncValidationOptions,
 ): Pipe<any, Promise<C | Joi.ValidationError>>;
 
 export function validatePipe(
-  schema: ClassSchema<object> | Joi.Schema,
+  schema: ClassSchema<object> | Joi.Schema | z.Schema,
+  options?: Partial<z.ParseParams> | ClassParams | Joi.AsyncValidationOptions,
 ): Pipe<any, Promise<any>> {
   if ('validateAsync' in schema) {
-    return pipe(async (obj: any) => {
+    return pipe(async (obj: unknown) => {
       try {
-        return await schema.validateAsync(obj);
+        return await schema.validateAsync(
+          obj,
+          options as Joi.AsyncValidationOptions,
+        );
       } catch (err) {
         return err;
       }
     });
+  } else if ('safeParseAsync' in schema) {
+    return pipe(async (obj: unknown) => {
+      const res = await schema.safeParseAsync(
+        obj,
+        options as Partial<z.ParseParams>,
+      );
+      if (res.success) return res.data;
+      else return res.error;
+    });
   } else {
-    return pipe(async (obj: any) => {
-      const validateObj: Record<string, any> = new schema();
-      for (const key in obj) {
-        validateObj[key] = obj[key];
-      }
-      const res = await validate(validateObj);
+    return pipe(async (obj: unknown) => {
+      const validateObj = plainToInstance(
+        schema,
+        obj,
+        (options as ClassParams)?.transform,
+      );
+      const res = await validate(
+        validateObj,
+        (options as ClassParams)?.validation,
+      );
       if (!res.length) return obj;
       return new ClassValidatorError(res);
     });
